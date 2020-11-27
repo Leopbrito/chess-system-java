837c8e65ae7ab0583a63586ee64af0b8153c4420
Leonardo Brito <leonardo.paes.brito@gmail.com>
2020-11-27 11:29:30 -0300

    starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,20 @@
+package boardgame;
+
+
+/**
+ * Piece
+ * @author Leopbrito
+ */
+public class Piece {
+    
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board){
+        this.board = board;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+}
